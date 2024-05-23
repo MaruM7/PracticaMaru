21865e2605dc3a436013977417df07613a1fcc73
Javier Montero <jmonterom20@educarex.es>
2024-05-23 14:02:31 +0200

    Update hola.java

--- hola.java
+++ hola.java
@@ -1,5 +1,5 @@
 public class hola {
   public static void main (String[] args) {
-    System.out.println("Hola, mundo, soy Felix :D");
+    System.out.println("Hola mundo! Soy Javier. uwunya");
   }
 }
